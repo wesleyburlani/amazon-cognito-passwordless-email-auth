42639763b701ae8c8958585c4c68736bbb2d1374
Antonio Anderson Souza <antonio@vizir.com.br>
2020-05-08 11:08:07 -0300

    Ajustes para fncionar no meu cenário de poc

--- client/src/app/answer-challenge/answer-challenge.component.ts
+++ client/src/app/answer-challenge/answer-challenge.component.ts
@@ -16,18 +16,8 @@ import { tap } from 'rxjs/operators';
 })
 export class AnswerChallengeComponent implements OnInit, OnDestroy, AfterContentInit {
 
-  digit1 = new FormControl('');
-  digit2 = new FormControl('');
-  digit3 = new FormControl('');
-  digit4 = new FormControl('');
-  digit5 = new FormControl('');
-  digit6 = new FormControl('');
+  oam_token = new FormControl('');
   @ViewChild('digit1el') digit1element: ElementRef;
-  @ViewChild('digit2el') digit2element: ElementRef;
-  @ViewChild('digit3el') digit3element: ElementRef;
-  @ViewChild('digit4el') digit4element: ElementRef;
-  @ViewChild('digit5el') digit5element: ElementRef;
-  @ViewChild('digit6el') digit6element: ElementRef;
 
   private errorMessage_ = new BehaviorSubject('');
   public errorMessage = this.errorMessage_.asObservable();
@@ -49,37 +39,6 @@ export class AnswerChallengeComponent implements OnInit, OnDestroy, AfterContent
     this.auth.getPublicChallengeParameters()
       .then(param => this.email_.next(param.email));
 
-    // Move focus to next field upon entry of a digit
-    [2, 3, 4, 5, 6].forEach(digit => {
-      const prev = this[`digit${digit - 1}`] as FormControl;
-      const next = this[`digit${digit}element`] as ElementRef;
-      this.allSubscriptions.add(prev.valueChanges.pipe(
-        tap(() => {
-          if (prev.value) {
-            next.nativeElement.focus();
-            next.nativeElement.setSelectionRange(0, 1);
-          }
-        })
-      ).subscribe());
-    });
-
-    // If the user copy pastes the code into the first digit field
-    // we'll be so kind to cut it in 6 pieces and distribute it to the right fields
-    this.allSubscriptions.add(
-      this.digit1.valueChanges.pipe(
-        tap(value => {
-          if (value && value.length > 1) {
-            const digits = value.split('').slice(0, 6);
-            this.digit1.setValue(digits[0]);
-            this.digit2.setValue(digits[1]);
-            this.digit3.setValue(digits[2]);
-            this.digit4.setValue(digits[3]);
-            this.digit5.setValue(digits[4]);
-            this.digit6.setValue(digits[5]);
-          }
-        })
-      ).subscribe()
-    );
   }
 
   ngOnDestroy() {
@@ -94,9 +53,7 @@ export class AnswerChallengeComponent implements OnInit, OnDestroy, AfterContent
     try {
       this.errorMessage_.next('');
       this.busy_.next(true);
-      const answer = [1, 2, 3, 4, 5, 6]
-        .map(digit => (this[`digit${digit}`] as FormControl).value)
-        .join('');
+      const answer = (this[`oam_token`] as FormControl).value
       const loginSucceeded = await this.auth.answerCustomChallenge(answer);
       if (loginSucceeded) {
         this.router.navigate(['/private']);
--- cognito/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
+++ cognito/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
@@ -1,14 +1,33 @@
+"use strict";
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+Object.defineProperty(exports, "__esModule", { value: true });
+const request = require('request');
 
-import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
-
-export const handler: CognitoUserPoolTriggerHandler = async event => {
-    const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode; 
-    if (event.request.challengeAnswer === expectedAnswer) {
+exports.handler = (event, context, callback) => {
+    console.log('Validating OAM Token on OAM');
+    console.log(event.request.challengeAnswer);
+      var headers = {
+        'Cookie': event.request.challengeAnswer,
+      };
+      var options = {
+        url: 'https://consultoria-stg1.natura.com.br/statuslogincogn/index.html',
+        method: 'GET',
+        headers: headers,
+        strictSSL: false,
+        insecure: true,
+        followRedirect: false,
+        followAllRedirects: false
+      };
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
         event.response.answerCorrect = true;
-    } else {
+        console.log('OAM Token validated');
+        callback(null, event);
+      } else {
         event.response.answerCorrect = false;
-    }
-    return event;
+        console.log('OAM Token invalid');
+        callback(null, event);
+      }
+    });
 };
